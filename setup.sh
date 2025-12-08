43c661be1589673b0908d3b32390f3286b37d928
StijnvdMade <stijnmade@gmail.com>
2025-12-08 16:20:51 +0100

    Fix CTFd Whale plugin repository URL in setup script

--- setup.sh
+++ setup.sh
@@ -40,7 +40,7 @@ docker node update --label-add='name=linux-1' $(docker node ls -q)
 # Clone the CTFd whale repo if it doesn't exist
 if [ ! -d "CTFd/plugins/ctfd_whale" ]; then
     echo "Cloning CTFd-Whale plugin..."
-    git clone https://github.com/frankli0324/ctfd-whale.git CTFd/plugins/ctfd_whale
+    git clone https://github.com/StijnvdMade/ctfd-whale.git CTFd/plugins/ctfd_whale
 else
     echo "CTFd-Whale plugin already exists, skipping clone"
 fi
